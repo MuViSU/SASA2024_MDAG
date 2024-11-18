0dc49645b09ac29a4ee5e350cddcd556814c4f51
Johane NienkemperSwanepoel <nienkemperj@sun.ac.za>
2024-11-18 10:24:37 +0200

    CA script

--- scripts/2_MDAG_CA_code.R
+++ scripts/2_MDAG_CA_code.R
@@ -5,7 +5,7 @@
 #18 November 2024
 ######################################################
 
-## ----------------------------------------------------------------------------------------------------------------------
+## Contingency table
 cross_tab <- HairEyeColor[,,2]
 cross_tab
 
@@ -13,17 +13,17 @@ cross_tab
 biplot(HairEyeColor[,,2], center = TRUE) |> CA()
 
 
-## ----------------------------------------------------------------------------------------------------------------------
+## Correspondence table
 P_mat <- cross_tab/sum(cross_tab)
 P_mat
 
 
-## ----------------------------------------------------------------------------------------------------------------------
+## Row masses
 ca.out <- biplot(HairEyeColor[,,2], center = FALSE) |> CA()
 ca.out$Dr
 
 
-## ----------------------------------------------------------------------------------------------------------------------
+## Column masses
 ca.out <- biplot(HairEyeColor[,,2], center = FALSE) |> CA()
 ca.out$Dc
 
@@ -31,33 +31,37 @@ ca.out$Dc
 # CA()
 
 
-## ----------------------------------------------------------------------------------------------------------------------
+## First CA plot: default arguments
 biplot(HairEyeColor[,,2], center = FALSE) |> 
   CA() |> 
   plot()
 
 
-## ----------------------------------------------------------------------------------------------------------------------
+## Symmetric variant with a legend
 biplot(HairEyeColor[,,2], center = FALSE) |> 
-  CA(variant = "Symmetric") |> samples(cex = 1.5) |> 
-  legend.type(samples = TRUE) |> 
+  CA(variant = "Symmetric") |> samples(col = c("darkmagenta", "forestgreen"), 
+                                       cex = 1.7, opacity = 0.7) |> legend.type(samples = TRUE) |> plot()
+
+
+## Standard variant
+biplot(HairEyeColor[,,2], center = FALSE) |> CA(variant = "Stand") |> 
+  samples(col = c("darkmagenta", "forestgreen"), cex = 1.7, opacity = 0.7) |> 
   plot()
 
 
-## ----------------------------------------------------------------------------------------------------------------------
+## Standard variant with and without lambda scaling
+par(mfrow=c(1,2))
 biplot(HairEyeColor[,,2], center = FALSE) |> 
-  CA(variant = "Stand") |> samples(cex = 1.5) |> 
+  CA(variant = "Stand", lambda.scal = FALSE) |>
+  samples(col=c("palevioletred3","purple4"), cex = 1.7, opacity = 0.7) |> 
   plot()
-
-
-## ----------------------------------------------------------------------------------------------------------------------
 biplot(HairEyeColor[,,2], center = FALSE) |> 
   CA(variant = "Stand", lambda.scal = TRUE) |>
-  samples(col=c("palevioletred1","purple4"), cex = 1.5) |> 
+  samples(col=c("palevioletred3","purple4"), cex = 1.7, opacity = 0.7) |> 
   plot()
 
 
-## ----------------------------------------------------------------------------------------------------------------------
+## Interpolating new samples
 biplot(HairEyeColor[,,2], center = FALSE) |> CA(variant = "Symmetric") |>  
   samples(pch = c(0,2), cex = 1.5) |> 
   interpolate(newdata = HairEyeColor[,,1]) |> 
@@ -65,7 +69,7 @@ biplot(HairEyeColor[,,2], center = FALSE) |> CA(variant = "Symmetric") |>
   plot()    
 
 
-## ----------------------------------------------------------------------------------------------------------------------
+## Evaluating the fit measures
 ca.out <- biplot(HairEyeColor[,,2], center = FALSE) |> 
   CA(variant = "Symmetric") |> 
   fit.measures()
@@ -80,14 +84,23 @@ ca.out$adequacy
 
 
 ## ----------------------------------------------------------------------------------------------------------------------
-ca.out <- biplot(HairEyeColor[,,2], center = FALSE) |> 
-  CA(variant = "Symmetric") |> 
-  fit.measures()
-
-
-## ----------------------------------------------------------------------------------------------------------------------
 ca.out$row.predictivities
 
 
 ## ----------------------------------------------------------------------------------------------------------------------
 ca.out$col.predictivities
+
+
+## Additional (not in slides)
+
+## 1D biplot
+biplot(HairEyeColor[,,2], center = FALSE) |> 
+  CA(variant = "Stand", lambda.scal = TRUE, dim.biplot = 1) |>
+  samples(col=c("darkmagenta", "forestgreen"), cex = 1.5) |> 
+  plot()
+
+## 3D biplot
+biplot(HairEyeColor[,,2], center = FALSE) |> 
+  CA(variant = "Stand", lambda.scal = TRUE, dim.biplot = 3) |>
+  samples(col=c("darkmagenta", "forestgreen"), cex = 1.5) |> 
+  plot()
